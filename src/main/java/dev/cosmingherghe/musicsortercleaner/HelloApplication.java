03f0cd25995af30726c7a0676451522171034d01
cosmingherghe <cosminlinux@gmail.com>
2023-04-30 21:55:20 -0400

    Add CSS Styling

--- src/main/java/dev/cosmingherghe/musicsortercleaner/HelloApplication.java
+++ src/main/java/dev/cosmingherghe/musicsortercleaner/HelloApplication.java
@@ -12,6 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 1000);
+
         stage.setTitle("Music Cleaner");
         stage.setScene(scene);
         stage.show();
